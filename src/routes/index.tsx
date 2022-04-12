9e7ec22cb651541bab4866c254bd58c44b500103
erika0liveira <erikaalves018@gmail.com>
2022-04-12 15:21:31 -0300

    material ui and themes setup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<p>Home</p>}/>
+            <Route path="/" element={<Button variant='contained' color='secondary'>Botão</Button>}/>
 
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+import { cyan, teal } from "@mui/material/colors";
+
+export const LightTheme = createTheme({
+    palette: {
+        primary: {
+            light: cyan[500],
+            main: cyan[700],
+            dark: cyan[800],
+            contrastText: '#ffffff',
+        },
+        secondary: {
+            light: teal[200],
+            main: teal[300],
+            dark: teal[400],
+            contrastText: '#ffffff',
+        },
+        background: {
+            default: 'f7f6f3',
+            paper: 'ffffff',
+        }
+    }
+});
