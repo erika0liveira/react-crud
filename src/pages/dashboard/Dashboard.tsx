91fda254a97d395172be92eef09ffe6a768d78e8
erika0liveira <erikaalves018@gmail.com>
2022-04-14 19:22:22 -0300

    toolbar component setup

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
+import { Toolbar } from "../../shared/components";
 import { LayoutBase } from "../../shared/layouts";
 
 export const Dashboard = () => {
 
   return (
-    <LayoutBase title="Página inicial" toolBar={<>barra</>}>
+    <LayoutBase title="Página inicial" toolBar={(<Toolbar showSearchInput/>)}>
         Teste
     </LayoutBase>
   );
---
+++ src/shared/components/toolbar/Toolbar.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, Icon, Paper, TextField, useTheme } from "@mui/material";
+
+interface IToolbarProps {
+  searchText?: string;
+  showSearchInput?: boolean;
+  changeSearchText?: (newText: string) => void;
+  btnText?: string;
+  showNewBtn?: boolean;
+  newOnClick?: () => void;
+}
+
+export const Toolbar: React.FC<IToolbarProps> = ({
+  searchText = "",
+  showSearchInput = false,
+  changeSearchText,
+  btnText = "Novo",
+  showNewBtn = true,
+  newOnClick,
+}) => {
+  const theme = useTheme();
+
+  return(
+    <Box 
+      component={Paper}
+      display="flex" 
+      gap={1}
+      alignItems="center"
+      height={theme.spacing(5)} 
+      padding={1}
+      marginX={1}
+      paddingX={1}
+    >
+      { showSearchInput && (
+        <TextField 
+          size="small" 
+          placeholder="Pesquisar..."
+          value={searchText}
+          onChange={(e) => changeSearchText?.(e.target.value)}
+        />
+      )}
+
+      <Box
+        display="flex"
+        justifyContent="end"
+        flex={1}
+      >{showNewBtn &&(
+          <Button
+            variant="contained"
+            color="primary"
+            disableElevation
+            endIcon={<Icon>add</Icon>}
+            onClick={newOnClick}
+          >{btnText}</Button>
+        )}
+      </Box>
+
+    </Box>
+  );
+};
