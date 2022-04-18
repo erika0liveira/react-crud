b3eb042c0259e3d07346385954bff8ffba43d471
erika0liveira <erikaalves018@gmail.com>
2022-04-18 16:27:54 -0300

    ADD: environmental vars

--- src/shared/components/listing-tool/ListingTool.tsx
+++ src/shared/components/listing-tool/ListingTool.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Icon, Paper, TextField, useTheme } from "@mui/material";
 
+import { Environment } from "../../environment";
+
 interface IListingToolProps {
   searchText?: string;
   showSearchInput?: boolean;
@@ -33,7 +35,7 @@ export const ListingTool: React.FC<IListingToolProps> = ({
       { showSearchInput && (
         <TextField 
           size="small" 
-          placeholder="Pesquisar..."
+          placeholder={Environment.SEARCH_INPUT}
           value={searchText}
           onChange={(e) => changeSearchText?.(e.target.value)}
         />
--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,6 @@
-export {};
+export const Environment = {
+  ROW_LIMIT: 10,
+  SEARCH_INPUT: "Pesquisar...",
+  EMPTY_LISTING: "Nenhum registro encontrado.",
+  URL_BASE: "http://localhost:3333"
+};
