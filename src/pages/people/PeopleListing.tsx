5b8791d607098c516ef8a2c6d1c839c3d54d20d5
erika0liveira <erikaalves018@gmail.com>
2022-04-22 14:17:23 -0300

    ADD: debounce hook

--- src/pages/people/PeopleListing.tsx
+++ src/pages/people/PeopleListing.tsx
@@ -2,25 +2,29 @@ import { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import { ListingTool } from "../../shared/components";
+import { useDebounce } from "../../shared/hooks";
 import { LayoutBase } from "../../shared/layouts";
 import { PeopleServices } from "../../shared/services/api/people/PeopleServices";
 
 export const PeopleListing: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
 
   const search = useMemo(() => {
     return searchParams.get("search") || "";
   }, [searchParams]);
 
   useEffect(() => {
-    PeopleServices.getAll(1, search)
-      .then((result) => {
-        if(result instanceof Error){
-          alert(result.message);
-        } else {
-          console.log(result);
-        }
-      });
+    debounce(() => {
+      PeopleServices.getAll(1, search)
+        .then((result) => {
+          if(result instanceof Error){
+            alert(result.message);
+          } else {
+            console.log(result);
+          }
+        });
+    });
   }, [search]);
 
   return (
@@ -35,7 +39,7 @@ export const PeopleListing: React.FC = () => {
         />
       }
     >
-      aaaaaaaaaaaaaaaaaaa
+      Listagem de Pessoas :-)
     </LayoutBase>
   );
 };
